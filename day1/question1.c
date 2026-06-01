ae3495c0651a7e4c7d51cdfbea3726ad8f73c1f1
Shwetay006 <shweta052006@gmail.com>
2026-06-01 21:29:35 +0530

    Add files via upload

---
+++ day1/question1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,sum=0;
+     
+    printf("enter a  natural number");
+    scanf("%d",&n);
+
+
+    for(i=1;i<=n;i++)
+    {
+    sum = sum+i;
+    }
+    
+        printf("%d\n",sum);
+    
+
+    return 0;
+}
---
+++ day1/question3.c
@@ -0,0 +1,21 @@
+// factorial of the number
+
+#include<stdio.h>
+int main()
+{
+    int n,i,fact=1;
+
+    printf("enter a number");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+   {
+    fact = fact*i;
+
+   } 
+    
+   printf("the factorical  is %d =%d\n",n,fact);
+
+   return 0;
+
+}
